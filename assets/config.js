e7e1624b5c171efd3aa070a65bea99afede22e0d
Daniel Facini Kiffer <danielfkiffer@outlook.com>
2022-07-25 20:17:30 -0300

    Add files via upload

---
+++ assets/config.js
@@ -0,0 +1,37 @@
+let config = {
+  "Title": "Daniel - Links",
+  "Name": "Daniel Kiffer",
+  "Description": "offensive security student",
+  "links": [
+    {
+      "Title": "Website",
+      "URL": "http://www.harindu.dev",
+      "icon_classes": "fas fa-link",
+    },
+    {
+      "Title": "Github",
+      "URL": "https://github.com/danielfk11",
+      "icon_classes": "fab fa-github",
+    },
+    {
+      "Title": "Email",
+      "URL": "mailto:danielfkiffer@outlook.com",
+      "icon_classes": "fas fa-envelope"
+    },
+    {
+      "Title": "Twitter",
+      "URL": "https://twitter.com/crfdanielkiffer",
+      "icon_classes": "fab fa-twitter"
+    },
+    {
+      "Title": "Instagram",
+      "URL": "https://instagram.com/danielfkiffer",
+      "icon_classes": "fab fa-instagram"
+    },
+    {
+      "Title": "LinkedIn",
+      "URL": "https://www.linkedin.com/in/daniel-kiffer-320820234/",
+      "icon_classes": "fab fa-linkedin"
+    }
+  ]
+}
